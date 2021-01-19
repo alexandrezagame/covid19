de6020f95218d391a562e0b1e0484a124acb6d22
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-19 14:53:25 +0100

    functional component in App js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
@@ -6,35 +6,34 @@ import { fetchData } from "./api";
 
 import covidlogo from "./images/covidlogo.png";
 
-class App extends React.Component {
-  state = {
-    data: {},
-    country: "",
-  };
+const App = () => {
+  const [data, setData] = useState({});
+  const [country, setCountry] = useState("");
 
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
+  useEffect(() => {
+    const fetchedAPI = async () => {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+    };
+    fetchedAPI();
+  }, []);
 
-  handleCountryChange = async (country) => {
+  const handleCountryChange = async (country) => {
     // fetch the data
     const fetchedData = await fetchData(country);
     // set the state
-    this.setState({ data: fetchedData, country: country });
+    setData(fetchedData);
+    setCountry(country);
   };
 
-  render() {
-    const { data, country } = this.state;
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={covidlogo} alt="Covid 19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.container}>
+      <img className={styles.image} src={covidlogo} alt="Covid 19" />
+      <Cards data={data} />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Chart data={data} country={country} />
+    </div>
+  );
+};
 
 export default App;
